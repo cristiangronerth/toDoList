f40653366c9cb8f006b71be68b60806255e41beb
cristiangronerth <cristiangronerth@gmail.com>
2023-06-10 15:39:25 -0300

    navbar funcionando

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,12 +1,24 @@
-import { Button } from '@mui/material';
-
+import { AppBar, Box, Button, Container, Toolbar, Typography } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+
+  const navigate = useNavigate()
+
   return (
-    <div>
-      Navbar
-      <Button variant='contained'color='success'>Click Me</Button>
-    </div>
+    <Box sx={{flexGrow: 1 }}>  {/* en lugar de div */}
+      <AppBar position='static' color='transparent'> {/* es para ener una navegacion */}
+        <Container> {/* para que este centrado */}
+          <Toolbar> {/* para colocar las opcines */}
+            <Typography variant='h6' sx={{flexGrow: 1 }}>  {/* para colocar los textos dentro de mui */}
+              <Link to="/" style={{textDecoration:'none', color:'#eee'}}>PERN STACK</Link> {/* para navegar */}
+            </Typography>
+            <Button variant='contained' color='primary' onClick={()=>navigate("/tasks/new")}>New Task</Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
+
   )
 }
 
